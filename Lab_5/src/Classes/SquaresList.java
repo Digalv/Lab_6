2ee088ff717c8de93c658512f512124f1a84877b
Digalv <100136401+Digalv@users.noreply.github.com>
2022-05-18 14:30:57 +0200

    Add files via upload

---
+++ Lab_5/src/Classes/SquaresList.java
@@ -0,0 +1,39 @@
+package Classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SquaresList {
+    private final List<Square> squareList;
+
+    public SquaresList() {
+        this.squareList = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "SquaresList: " + squareList;
+    }
+
+    public boolean add(Square square) {
+        return this.squareList.add(square);
+    }
+
+    public void remove(int number) {
+        if (number >= 0 && number < this.squareList.size()) {
+            this.squareList.remove(number);
+        }
+    }
+
+    public void remove(Square square) {
+        this.squareList.remove(square);
+    }
+
+    public float getAvgS() {
+        float sum = 0;
+        for (Square square : this.squareList) {
+            sum += square.getSquare();
+        }
+        return sum / this.squareList.size();
+    }
+}
